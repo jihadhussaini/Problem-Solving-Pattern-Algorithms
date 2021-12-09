72eb3a15088b637249c682ced9b5c931119dfad0
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2021-12-09 16:40:29 +0700

    additional note for question on each problem

--- frequency_counter/anagrams/anagrams.js
+++ frequency_counter/anagrams/anagrams.js
@@ -1,3 +1,22 @@
+/*
+Frequency Counter - validAnagram
+
+Given two strings, write a function to determine if the second string is an anagram of the
+first. An anagram is a word, phrase, or name formed by rearranging the letters of another,
+such as cinema, formed from iceman.
+Examples:
+validAnagram('','') / true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram','nagaram' ) // true
+validAnagram("rat", "car") // false) // false
+validAnagram('awesome',"awesom") // false
+validAnagram(' amanaplanacanalpanama', 'acanalmanplanpamana" )// false
+validAnagram(' qwerty', 'geywrt") // true
+validAnagram('texttwisttime', "timetwisttext') // true
+Note: You may assume the string contains only lowercase alphabets.
+Time Complexity - O(n)
+*/
+
 function validAnagrams (str1, str2) {
     // cek length
     if (str1.length !== str2.length) {
