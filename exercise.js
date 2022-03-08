3f49747aca2407e842ec0de945f674ea3707650f
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2022-03-08 10:20:47 +0700

    add two more exercise

--- exercise.js
+++ exercise.js
@@ -269,3 +269,23 @@ function isPalindrome(str){
   if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
   return false;
 }
+
+// some Recursive
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1),callback);
+}
+
+// Flatten
+function flatten(oldArr){
+  let newArr = []
+  	for(let i = 0; i < oldArr.length; i++){
+    	if(Array.isArray(oldArr[i])){
+      		newArr = newArr.concat(flatten(oldArr[i]))
+    	} else {
+      		newArr.push(oldArr[i])
+    	}
+  } 
+  return newArr;
+}
